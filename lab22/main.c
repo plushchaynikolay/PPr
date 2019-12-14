b50f4225d5d8d8e98f439202396c1537e69c97bb
Plyushchay Nikolay <n.plyushchay@napoleonit.ru>
2019-12-15 01:53:17 +0500

    fix 22

--- lab22/main.c
+++ lab22/main.c
@@ -5,29 +5,31 @@ int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
 
-    int rank;
+    int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    int size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPI_Status status;
-    MPI_Request request;
+    int dest, source;
+    int         recieved [size - 1];
+    MPI_Status  statuses [size - 1];
+    MPI_Request requests [size - 1];
 
     for (int i = 1; i < size; i++)
     {
-        MPI_Isend(&rank, 1, MPI_INT, (rank + i) % size, rank, MPI_COMM_WORLD, &request);
+        dest = (rank + i) % size;
+        MPI_Isend(&rank, 1, MPI_INT, dest, rank, MPI_COMM_WORLD, &requests[i - 1]);
     }
 
-    int recieved;
-    int source;
-    
     for (int i = 1; i < size; i++)
     {
         source = (rank + i) % size;
-        MPI_Irecv(&recieved, 1, MPI_INT, source, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
-        MPI_Wait(&request, &status);
-        printf("[%d] recieved `%d` from %d\n", rank, recieved, source);
+        MPI_Irecv(&recieved[i - 1], 1, MPI_INT, source, MPI_ANY_TAG, MPI_COMM_WORLD, &requests[i - 1]);
+    }
+
+    MPI_Waitall(size - 1, requests, statuses);
+    for (int i = 0; i < size - 1; i++){
+        source = (&statuses[i]) -> MPI_SOURCE;
+        printf("[%d] recieved `%d` from %d\n", rank, recieved[i], source);
     }
 
     MPI_Finalize();
